07991fb8e8023f48cd7b9c7885c08f02c9c51faa
Tom <tdedobbeleer@gmail.com>
2021-11-01 14:04:59 +0100

    catch exception

--- src/main/java/be/covid/stats/config/AppConfig.java
+++ src/main/java/be/covid/stats/config/AppConfig.java
@@ -2,20 +2,26 @@ package be.covid.stats.config;
 
 import be.covid.stats.services.CachedStatsService;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.EventListener;
-
-import java.io.IOException;
 
 @Configuration
 @AllArgsConstructor
 public class AppConfig {
     private final CachedStatsService cachedStatsService;
 
+    private static final Logger log = LoggerFactory.getLogger(AppConfig.class);
+
     //Fill cache after spring has loaded
     @EventListener(ApplicationReadyEvent.class)
-    public void doSomethingAfterStartup() throws IOException {
-        cachedStatsService.preloadCache();
+    public void doSomethingAfterStartup() {
+        try {
+            cachedStatsService.preloadCache();
+        } catch (Exception e) {
+            log.error("Could not preload data.");
+        }
     }
 }
